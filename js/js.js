67107f680da86025e712375cb7e5ea45f755cd38
wandersondv <wanderson.s.s1@gmail.com>
2022-01-20 06:01:27 -0300

    correção limitando movimento pra cima e pra baixo

--- js/js.js
+++ js/js.js
@@ -54,13 +54,18 @@ function start() { // Inicio da função start()
             if (jogo.pressionou[TECLA.W]) {
                 var topo = parseInt($("#jogador").css("top"));
                 $("#jogador").css("top",topo-10);
-            
+                    if (topo<=0) {
+                        $("#jogador").css("top",topo+10);
+                    }
             }
             
             if (jogo.pressionou[TECLA.S]) {
                 
                 var topo = parseInt($("#jogador").css("top"));
-                $("#jogador").css("top",topo+10);	
+                $("#jogador").css("top",topo+10);
+                    if (topo>=434) {	
+                        $("#jogador").css("top",topo-10);
+                    }
             }
             
             if (jogo.pressionou[TECLA.D]) {

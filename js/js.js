09b6f7f8e1223b56e9355c90cdb32305a40dafb3
wandersondv <wanderson.s.s1@gmail.com>
2022-01-20 06:45:58 -0300

    funcao colisao

--- js/js.js
+++ js/js.js
@@ -43,6 +43,7 @@ function start() { // Inicio da função start()
     moveinimigo1();
     moveinimigo2();
     moveamigo();
+    colisao();
 	
 	} // Fim da função loop()
 
@@ -146,10 +147,16 @@ function start() { // Inicio da função start()
                     podeAtirar=true;
                             
                            }
-            } // Fecha executaDisparo()
+        } // Fecha executaDisparo()
+
         } // Fecha disparo()
+        function colisao() {
+            var colisao1 = ($("#jogador").collision($("#inimigo1")));
+            // jogador com o inimigo1
+        
+            console.log(colisao1);
+        
+        } //Fim da função colisao()
         
         
-        
-    
 } // Fim da função start

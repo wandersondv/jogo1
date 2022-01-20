b35f570ca5d526ef43f9c15d60d31f1c594ad702
wandersondv <wanderson.s.s1@gmail.com>
2022-01-20 04:55:29 -0300

    funcao start

---
+++ js/js.js
@@ -0,0 +1,11 @@
+
+function start() { // Inicio da fun��o start()
+
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+
+} // Fim da fun��o start

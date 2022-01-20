b59c9f8188b7f68f89dc5d354d68cadbd6b620dc
wandersondv <wanderson.s.s1@gmail.com>
2022-01-20 06:35:27 -0300

    funcao de disparo

--- js/js.js
+++ js/js.js
@@ -12,6 +12,7 @@ function start() { // Inicio da função start()
 	
 	var jogo = {};//objeto
     var velocidade=5;
+    var podeAtirar=true;
     var posicaoY = parseInt(Math.random() * 334);
     var TECLA = {
         W: 87,
@@ -75,7 +76,8 @@ function start() { // Inicio da função start()
             
             if (jogo.pressionou[TECLA.D]) {
                 
-                //Chama função Disparo	
+                //Chama função Disparo
+                disparo();
             }
         
             } // fim da função movejogador()
@@ -114,6 +116,39 @@ function start() { // Inicio da função start()
                 }
         
         } // fim da fun��o moveamigo()
+        function disparo() {
+	
+            if (podeAtirar==true) {
+                
+            podeAtirar=false;
+            
+            topo = parseInt($("#jogador").css("top"))
+            posicaoX= parseInt($("#jogador").css("left"))
+            tiroX = posicaoX + 190;
+            topoTiro=topo+37;
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top",topoTiro);
+            $("#disparo").css("left",tiroX);
+            
+            var tempoDisparo=window.setInterval(executaDisparo, 30);
+            
+            } //Fecha podeAtirar
+         
+                   function executaDisparo() {
+                posicaoX = parseInt($("#disparo").css("left"));
+                $("#disparo").css("left",posicaoX+15); 
+        
+                        if (posicaoX>900) {
+                                
+                    window.clearInterval(tempoDisparo);
+                    tempoDisparo=null;
+                    $("#disparo").remove();
+                    podeAtirar=true;
+                            
+                           }
+            } // Fecha executaDisparo()
+        } // Fecha disparo()
+        
         
         
     
